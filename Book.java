6e75134127922746d21dfe26a9a553f392afad6c
JohnPhilip80 <rushtophilip@gmail.com>
2025-07-22 12:16:05 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,47 +1,70 @@
-package com.mrk.bala.bookapplication.bookproject.models;
+package com.mrk.john.bookapplication.bookproject.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="book")
 public class Book {
-private Long bno;
-private String title;
-private String author;
-private Double price;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long bno;
+	private String title;
+	private String author;
+	private Double price;
 
+	public Book(String title, String author, Double price) {
+		super();
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
 
-public Book(String title, String author, Double price) {
-super();
-this.title = title;
-this.author = author;
-this.price = price;
-}
-public Book(Long bno, String title, String author, Double price) {
-super();
-this.bno = bno;
-this.title = title;
-this.author = author;
-this.price = price;
-}
-public Long getBno() {
-return bno;
-}
-public void setBno(Long bno) {
-this.bno = bno;
-}
-public String getTitle() {
-return title;
-}
-public void setTitle(String title) {
-this.title = title;
-}
-public String getAuthor() {
-return author;
-}
-public void setAuthor(String author) {
-this.author = author;
-}
-public Double getPrice() {
-return price;
-}
-public void setPrice(Double price) {
-this.price = price;
-}
+	public Book(Long bno, String title, String author, Double price) {
+		super();
+		this.bno = bno;
+		this.title = title;
+		this.author = author;
+		this.price = price;
+	}
+
+	public Long getBno() {
+		return bno;
+	}
+
+	public void setBno(Long bno) {
+		this.bno = bno;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Book() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 }
